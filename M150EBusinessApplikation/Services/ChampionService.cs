be905e1cbd403dd1716a3f8a4cc09371bb0288be
Joel Muntwyler <joelmuntwyler@synus.ch>
2023-11-02 17:17:54 +0100

    Änderung: Shop bleibt mit Local Storage Blazor Lifecycle funktioniert damit

--- M150EBusinessApplikation/Services/ChampionService.cs
+++ M150EBusinessApplikation/Services/ChampionService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 using System.Security.Claims;
 using M150EBusinessApplikation.Data;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop;
 
 namespace M150EBusinessApplikation.Services
 {
@@ -29,13 +31,29 @@ namespace M150EBusinessApplikation.Services
                     var userChamp = Db.UserGameChampions.FirstOrDefault(t => t.ChampionId == champ.Id && t.Username == name);
                     if (userChamp != null) continue;
 
-                    var bought = (champ.Id <= 16 && champ.Id >= 1) ? true : false;
+                    var bought = champ.Id is <= 16 and >= 1;
 
                     if (name != null) Db.UserGameChampions.Add(new UserGameChampion { ChampionId = champ.Id, Username = name, Hidden = false, Bought = bought });
                     Db.SaveChanges();
                 }
             }
 
+        }
+        public async Task RestartGame(List<Champion> champions, IJSRuntime JsRuntime,string? name)
+        {
+            foreach (var champ in champions)
+            {
+                var userChamp = Db.UserGameChampions.FirstOrDefault(x => x.ChampionId == champ.Id && x.Username == name);
+                if (userChamp != null)
+                {
+                    userChamp.Hidden = false;
+                    userChamp.Chosen = false;
+                    Db.Entry(userChamp).State = EntityState.Modified;
+                }
+            }
+            await Db.SaveChangesAsync();
+            await JsRuntime.InvokeVoidAsync("flipAll");
+            await JsRuntime.InvokeVoidAsync("resultOutput", null);
         }
 
 
