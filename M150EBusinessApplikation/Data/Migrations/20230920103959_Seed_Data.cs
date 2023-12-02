a27ee869d1513be71e201a42816ff306741cbe1f
Jomu <joel.muntwyler@gmx.ch>
2023-12-02 17:01:19 +0100

    Update Seed Data

--- M150EBusinessApplikation/Data/Migrations/20230920103959_Seed_Data.cs
+++ M150EBusinessApplikation/Data/Migrations/20230920103959_Seed_Data.cs
@@ -14,69 +14,69 @@ namespace M150EBusinessApplikation.Data.Migrations
 GO
 SET IDENTITY_INSERT [dbo].[Champions] ON 
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (1, N'Aatrox', N'images\\icons\\Aatrox.png', N'Runeterra', N'Male', N'Melee', N'Manaless')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (1, N'Aatrox', N'images\\icons\\Aatrox.png', N'Runeterra', N'Male', N'Melee', N'Manaless', 20)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (2, N'Ahri', N'images\\icons\\Ahri.png', N'Ionia', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (2, N'Ahri', N'images\\icons\\Ahri.png', N'Ionia', N'Female', N'Ranged', N'Mana', 45)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (3, N'Akali', N'images\\icons\\Akali.png', N'Ionia', N'Female', N'Melee', N'Energy')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (3, N'Akali', N'images\\icons\\Akali.png', N'Ionia', N'Female', N'Melee', N'Energy', 25)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (4, N'Akshan', N'images\\icons\\Akshan.png', N'Shurima', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (4, N'Akshan', N'images\\icons\\Akshan.png', N'Shurima', N'Male', N'Ranged', N'Mana', 10)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (5, N'Alistar', N'images\\icons\\Alistar.png', N'Runeterra', N'Male', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (5, N'Alistar', N'images\\icons\\Alistar.png', N'Runeterra', N'Male', N'Melee', N'Mana', 15)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (6, N'Amumu', N'images\\icons\\Amumu.png', N'Shurima', N'Male', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (6, N'Amumu', N'images\\icons\\Amumu.png', N'Shurima', N'Male', N'Melee', N'Mana', 20)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (7, N'Anivia', N'images\\icons\\Anivia.png', N'Freljord', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (7, N'Anivia', N'images\\icons\\Anivia.png', N'Freljord', N'Female', N'Ranged', N'Mana', 25)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (8, N'Annie', N'images\\icons\\Annie.png', N'Runeterra', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (8, N'Annie', N'images\\icons\\Annie.png', N'Runeterra', N'Female', N'Ranged', N'Mana', 10)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (9, N'Aphelios', N'images\\icons\\Aphelios.png', N'Targon', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (9, N'Aphelios', N'images\\icons\\Aphelios.png', N'Targon', N'Male', N'Ranged', N'Mana', 15)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (10, N'Ashe', N'images\\icons\\Ashe.png', N'Freljord', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (10, N'Ashe', N'images\\icons\\Ashe.png', N'Freljord', N'Female', N'Ranged', N'Mana', 10)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (11, N'Aurelion Sol', N'images\\icons\\AurelionSol.png', N'Runeterra', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (11, N'Aurelion Sol', N'images\\icons\\AurelionSol.png', N'Runeterra', N'Male', N'Ranged', N'Mana', 30)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (12, N'Azir', N'images\\icons\\Azir.png', N'Shurima', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (12, N'Azir', N'images\\icons\\Azir.png', N'Shurima', N'Male', N'Ranged', N'Mana', 35)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (13, N'Bard', N'images\\icons\\Bard.png', N'Runeterra', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (13, N'Bard', N'images\\icons\\Bard.png', N'Runeterra', N'Male', N'Ranged', N'Mana', 50)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (14, N'Blitzcrank', N'images\\icons\\Blitzcrank.png', N'Zaun', N'Other', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (14, N'Blitzcrank', N'images\\icons\\Blitzcrank.png', N'Zaun', N'Other', N'Melee', N'Mana', 25)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (15, N'Brand', N'images\\icons\\Brand.png', N'Runeterra', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (15, N'Brand', N'images\\icons\\Brand.png', N'Runeterra', N'Male', N'Ranged', N'Mana', 15)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (16, N'Braum', N'images\\icons\\Braum.png', N'Freljord', N'Male', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (16, N'Braum', N'images\\icons\\Braum.png', N'Freljord', N'Male', N'Melee', N'Mana', 20)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (17, N'Caitlyn', N'images\\icons\\Caitlyn.png', N'Piltover', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (17, N'Caitlyn', N'images\\icons\\Caitlyn.png', N'Piltover', N'Female', N'Ranged', N'Mana', 25)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (18, N'Camille', N'images\\icons\\Camille.png', N'Piltover', N'Female', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (18, N'Camille', N'images\\icons\\Camille.png', N'Piltover', N'Female', N'Melee', N'Mana', 30)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (19, N'Cassiopeia', N'images\\icons\\Cassiopeia.png', N'Noxus', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (19, N'Cassiopeia', N'images\\icons\\Cassiopeia.png', N'Noxus', N'Female', N'Ranged', N'Mana', 20)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (20, N'Cho''gath', N'images\\icons\\Chogath.png', N'The Void', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (20, N'Cho''gath', N'images\\icons\\Chogath.png', N'The Void', N'Male', N'Ranged', N'Mana', 15)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (21, N'Corki', N'images\\icons\\Corki.png', N'Bandle City', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (21, N'Corki', N'images\\icons\\Corki.png', N'Bandle City', N'Male', N'Ranged', N'Mana', 10)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (22, N'Darius', N'images\\icons\\Darius.png', N'Noxus', N'Male', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (22, N'Darius', N'images\\icons\\Darius.png', N'Noxus', N'Male', N'Melee', N'Mana', 30)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (23, N'Diana', N'images\\icons\\Diana.png', N'Targon', N'Female', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (23, N'Diana', N'images\\icons\\Diana.png', N'Targon', N'Female', N'Melee', N'Mana', 30)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (24, N'Dr. Mundo', N'images\\icons\\DrMundo.png', N'Zaun', N'Male', N'Melee', N'Health costs')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (24, N'Dr. Mundo', N'images\\icons\\DrMundo.png', N'Zaun', N'Male', N'Melee', N'Health costs', 20)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (25, N'Draven', N'images\\icons\\Draven.png', N'Noxus', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (25, N'Draven', N'images\\icons\\Draven.png', N'Noxus', N'Male', N'Ranged', N'Mana', 25)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (26, N'Ekko', N'images\\icons\\Ekko.png', N'Zaun', N'Male', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (26, N'Ekko', N'images\\icons\\Ekko.png', N'Zaun', N'Male', N'Melee', N'Mana', 10)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (27, N'Elise', N'images\\icons\\Elise.png', N'Shadow Isles', N'Female', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (27, N'Elise', N'images\\icons\\Elise.png', N'Shadow Isles', N'Female', N'Ranged', N'Mana', 10)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (28, N'Evelynn', N'images\\icons\\Evelynn.png', N'Runeterra', N'Female', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (28, N'Evelynn', N'images\\icons\\Evelynn.png', N'Runeterra', N'Female', N'Melee', N'Mana', 45)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (29, N'Ezreal', N'images\\icons\\Ezreal.png', N'Piltover', N'Male', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (29, N'Ezreal', N'images\\icons\\Ezreal.png', N'Piltover', N'Male', N'Ranged', N'Mana', 35)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (30, N'Fiddlesticks', N'images\\icons\\Fiddlesticks.png', N'Runeterra', N'Other', N'Ranged', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (30, N'Fiddlesticks', N'images\\icons\\Fiddlesticks.png', N'Runeterra', N'Other', N'Ranged', N'Mana', 20)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (31, N'Fiora', N'images\\icons\\Fiora.png', N'Demacia', N'Female', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (31, N'Fiora', N'images\\icons\\Fiora.png', N'Demacia', N'Female', N'Melee', N'Mana', 40)
 GO
-INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource]) VALUES (32, N'Fizz', N'images\\icons\\Fizz.png', N'Runeterra', N'Male', N'Melee', N'Mana')
+INSERT [dbo].[Champions] ([Id], [Name], [ImageUrl], [Region], [Gender], [CombatStyle], [Resource], [Price]) VALUES (32, N'Fizz', N'images\\icons\\Fizz.png', N'Runeterra', N'Male', N'Melee', N'Mana', 25)
 GO
 SET IDENTITY_INSERT [dbo].[Champions] OFF
 GO
@@ -147,8 +147,7 @@ GO
 INSERT [dbo].[Heads] ([Id], [Haircolor], [Headgear], [Other], [ChampionId]) VALUES (32, NULL, NULL, N'Fish ears', 32)
 GO
 SET IDENTITY_INSERT [dbo].[Heads] OFF
-GO
-");
+GO");
         }
 
         /// <inheritdoc />
